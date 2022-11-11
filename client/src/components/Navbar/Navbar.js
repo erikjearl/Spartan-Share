b1186827883c64482d5e9ee881360a72a999e0f8
jjessicas <jks179@case.edu>
2022-11-11 15:14:46 -0500

    changing up home button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,11 +8,10 @@ const Navbar = () => {
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
           <img className={classes.image} src={upload} alt="upload" height="50"></img>
-          <Typography className={classes.heading} variant="h2" align="center">SpartanShare</Typography>
+          <Link href='/' className={classes.heading} variant="h2" align="center">SpartanShare</Link>
           <div className={classes.space}></div>
-          <Link href='/' className={classes.link} variant="h4">Home</Link>
-          <Link href='/search' className={classes.link} variant="h4">Search</Link>
           <Link href='/form' className={classes.link} variant="h4">Form</Link>
+          <Link href='/search' className={classes.link} variant="h4">Search</Link>
       </AppBar>
     );
   };
