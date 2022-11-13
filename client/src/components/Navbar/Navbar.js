8e1aef32f8ca44b56082ce3de48b9d886485b2ce
erikjearl <erikjearl@gmail.com>
2022-11-13 16:20:00 -0500

    icons
    Adding icons with MUI v4-
    
    npm install @material-ui/core --force
    
    npm install @material-ui/icons --force

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { AppBar, Typography, Link} from '@material-ui/core'
 import upload from '../../images/upload.png';
 import useStyles from './styles';
+import SearchIcon from '@material-ui/icons/Search';
+import PublishIcon from '@material-ui/icons/Publish';
 
 const Navbar = () => {
     const classes = useStyles();
@@ -10,8 +12,9 @@ const Navbar = () => {
           <img className={classes.image} src={upload} alt="upload" height="50"></img>
           <Link href='/' className={classes.heading} variant="h2" align="center">SpartanShare</Link>
           <div className={classes.space}></div>
-          <Link href='/form' className={classes.link} variant="h4">Upload</Link>
-          <Link href='/search' className={classes.link} variant="h4">Search</Link>
+          <Link href='/form' className={classes.link} variant="h4"> <PublishIcon className={classes.icons}/> Upload </Link>
+          <Link href='/search' className={classes.link} variant="h4"> <SearchIcon className={classes.icons}/> Search </Link>
+          
       </AppBar>
     );
   };
--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -28,12 +28,16 @@ export default makeStyles( () => ({
       },
       link: {
         marginTop: '15px',
-        marginLeft: '35px',
+        marginLeft: '25px',
+        marginRight: '25px',
         color: 'rgba(10, 48, 78)',
         fontFamily: [
           '"Times New Roman"',
           'Arial',
           'sans-serif',
         ].join(','),
-      }
+      },
+      icons: {
+        transform: 'scale(1.5)',
+      },
 }));
