8cccf3f708d0ddb8b074d8ed01f891f49d57bbc6
Alex Wang <iamalex11303@gmail.com>
2022-11-19 16:54:33 -0500

    working layout of homepage complete (need to add containers for notes)

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,6 +2,8 @@ import useStyles from './styles'
 import React from 'react';
 import Notes from '../Notes/Notes';
 import { Container, Grow, Grid, Typography } from '@material-ui/core'
+import NavigateBeforeSharpIcon from '@material-ui/icons/NavigateBeforeSharp';
+import NavigateNextSharpIcon from '@material-ui/icons/NavigateNextSharp';
 
 
 const Home = () => {
@@ -12,24 +14,47 @@ const Home = () => {
     return (
       <Grow in>
         <Container>
-          // Intro blurb
-          <Grid container justifyContent="space-between" alignItems="stretch">
-            <Grid item xs={true}>
-              <Typography paragraph className={classes.blurb}>Blurb (Add later)</Typography>
+
+          <Grid container direction="column" justifyContent="flex-start" alignItems="stretch">
+            <Grid item xs>
+              <Typography variant="h3" className={classes.blurb}>What is SpartanShare?</Typography>
+            </Grid>
+            <Grid item xs>
+              <Typography paragraph className={classes.blurb}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non leo et magna tristique egestas nec a nisl. Mauris quis ipsum nisl. Sed ultricies urna eros, vel ornare urna molestie eu. Proin ullamcorper dictum luctus. In vitae dolor ac nulla luctus consectetur id in justo. Sed ultricies lectus id semper ullamcorper. Nam orci massa, interdum sed tortor id, rutrum fermentum justo. Nulla facilisi. Integer tempor augue pellentesque, consectetur eros id, porttitor eros. Pellentesque et felis tortor. Suspendisse vitae turpis at dolor convallis aliquam. Nam ipsum ex, hendrerit venenatis odio sit amet, pretium consequat risus. Vestibulum non ligula rhoncus, congue nulla sit amet, interdum metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc nec semper felis. </Typography>
             </Grid>
           </Grid>
-          // Popular Entries Section
-          <Grid container justifyContent="flex-start" alignItems="stretch">
-            <Grid item xs={true}>
-              <Typography variant="h2" className={classes.subheader}>Popular SubHeader (Rename later)</Typography>
+
+          <Grid container direction="column" justifyContent="space-around" alignItems="stretch">
+            <Grid container direction="column" justifyContent="flex-start" alignItems="stretch" spacing={3}>
+
+              <Grid item xs>
+                <Typography variant="h4" className={classes.subheader}>Popular</Typography>
+              </Grid>
+              <Grid item xs>
+                <Container>
+                  <Grid container justifyContent="space-evenly" alignItems="stretch">
+                    <Grid item xs><NavigateBeforeSharpIcon color="primary" fontSize="large" /></Grid>
+                    <Grid item xs={10}>ADD CONTAINER FOR NOTES HERE</Grid>
+                    <Grid item xs><NavigateNextSharpIcon color="primary" fontSize="large" /></Grid>
+                  </Grid>
+                </Container>
+              </Grid>
+
+              <Grid item xs>
+                <Typography variant="h4" className={classes.subheader}>Recently Uploaded</Typography>
+              </Grid>
+              <Grid item xs>
+                <Container>
+                  <Grid container justifyContent="space-evenly" alignItems="stretch">
+                    <Grid item xs><NavigateBeforeSharpIcon color="primary" fontSize="large" /></Grid>
+                    <Grid item xs={10}>ADD CONTAINER FOR NOTES HERE</Grid>
+                    <Grid item xs><NavigateNextSharpIcon color="primary" fontSize="large" /></Grid>
+                  </Grid>
+                </Container>
+              </Grid>
             </Grid>
           </Grid>
-          // Recently Uploaded Section
-          <Grid container justifyContent="flex-start" alignItems="stretch">
-            <Grid item xs={true}>
-              <Typography variant="h2" className={classes.subheader}>Recent SubHeader (Rename later)</Typography>
-            </Grid>
-          </Grid>
+
        </Container>
       </Grow>
     );
