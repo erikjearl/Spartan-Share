cb84f275fdd19c0188eb078336a635bbaf8d7040
Alex Wang <iamalex11303@gmail.com>
2022-11-19 15:54:12 -0500

    added blurb section and headers for popular and recently uploaded sections on the homepage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,19 +1,34 @@
+import useStyles from './styles'
 import React from 'react';
 import Notes from '../Notes/Notes';
-import { Container, Grow, Grid } from '@material-ui/core'
+import { Container, Grow, Grid, Typography } from '@material-ui/core'
+
 
 const Home = () => {
-
+  const classes = useStyles();
 
     // add this later to make text white: style="color:#FFFFFF"
 
     return (
       <Grow in>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-              <Grid item sm={12}>
-                <Notes />
-              </Grid>
+          // Intro blurb
+          <Grid container justifyContent="space-between" alignItems="stretch">
+            <Grid item xs={true}>
+              <Typography paragraph className={classes.blurb}>Blurb (Add later)</Typography>
+            </Grid>
+          </Grid>
+          // Popular Entries Section
+          <Grid container justifyContent="flex-start" alignItems="stretch">
+            <Grid item xs={true}>
+              <Typography variant="h2" className={classes.subheader}>Popular SubHeader (Rename later)</Typography>
+            </Grid>
+          </Grid>
+          // Recently Uploaded Section
+          <Grid container justifyContent="flex-start" alignItems="stretch">
+            <Grid item xs={true}>
+              <Typography variant="h2" className={classes.subheader}>Recent SubHeader (Rename later)</Typography>
+            </Grid>
           </Grid>
        </Container>
       </Grow>
---
+++ client/src/components/Home/styles.js
@@ -0,0 +1,14 @@
+import { red } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles( () => ({
+
+    blurb: {
+        color: 'white',
+    },
+    subheader: {
+        color: 'white',
+        width: '100%',
+    },
+    
+}));
