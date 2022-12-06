eb9017ecb7f9d6806bf2c40cb542a4084751fe29
erikjearl <erikjearl@gmail.com>
2022-12-05 22:38:19 -0500

    Open and download note
    note details component

---
+++ client/src/components/NoteDetails/styles.js
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  media: {
+    borderRadius: '20px',
+    objectFit: 'cover',
+    width: '100%',
+    maxHeight: '600px',
+    maxWidth:'45vw',
+  },
+  card: {
+    display: 'flex',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+    },
+  },
+  section: {
+    borderRadius: '20px',
+    margin: '10px',
+    flex: 1,
+  },
+  imageSection: {
+    marginLeft: '20px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0,
+    },
+  },
+}));
--- client/src/components/Notes/Note/Note.js
+++ client/src/components/Notes/Note/Note.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
 
 //import { likePost } from '../../../actions/notes'
 import useStyles from './styles';
 
 const Note = ({note}) => {
     const classes = useStyles();
+    const navigate = useNavigate();
+    const openNote = () => navigate(`/notes/${note._id}`);
     
     return(
         <Card className={classes.card}>
-            
+
+        
             <CardMedia className={classes.media} image={note.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={note.title}/>
             <div className={classes.overlay}>
             <Typography variant="h6">{note.type}</Typography>
                 <Typography variant="body1">{moment(note.createdAt).fromNow()}</Typography>
             </div>
+        <ButtonBase className ={classes.cardAction} onClick={openNote} >
             <CardContent >
                 <Typography variant='h4'>
                     {note.name}
@@ -27,6 +32,7 @@ const Note = ({note}) => {
                     {note.description}
                 </Typography>
             </CardContent>
+        </ButtonBase>
 
             <CardActions className={classes.actions}>
                 <Button>Like 👍</Button>
